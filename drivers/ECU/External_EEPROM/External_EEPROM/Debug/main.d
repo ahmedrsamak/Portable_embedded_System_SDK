6947e90fc76d9622bcdcbcf2c60dcf64899245ae
ahmedsamak <ahmedrsamak@gmail.com>
2018-09-13 20:25:22 +0200

    EDit eeprom files

--- drivers/ECU/External_EEPROM/External_EEPROM/Debug/main.d
+++ drivers/ECU/External_EEPROM/External_EEPROM/Debug/main.d
@@ -1,1 +1,42 @@
-main.d main.o: .././main.c
+main.d main.o: .././main.c .././LCD/hal_lcd.h \
+ .././LCD/../LCD/DIO/hal_dio.h .././LCD/../LCD/DIO/../../common.h \
+ .././LCD/../LCD/DIO/../../reg_addr.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
+ .././TWI/hal_twi.h .././TWI/../common.h .././TWI/../reg_addr.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h
+
+.././LCD/hal_lcd.h:
+
+.././LCD/../LCD/DIO/hal_dio.h:
+
+.././LCD/../LCD/DIO/../../common.h:
+
+.././LCD/../LCD/DIO/../../reg_addr.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+
+.././TWI/hal_twi.h:
+
+.././TWI/../common.h:
+
+.././TWI/../reg_addr.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
--- drivers/ECU/External_EEPROM/External_EEPROM/main.c
+++ drivers/ECU/External_EEPROM/External_EEPROM/main.c
@@ -4,12 +4,45 @@
  * Created: 9/13/2018 10:17:23 AM
  * Author : ahmed
  */ 
-
-
-
+#include "LCD/hal_lcd.h"
+#include "TWI/hal_twi.h"
+#include <stdlib.h>
+config_lcd_pin LCD1;
+char _data[7];
 int main(void)
 {
-   
+	itoa(16,_data,10);
+	//char _data;
+	//_data=int_to_str(15,2);
+	LCD1.ctrl_port=PORTB;
+	LCD1.data_port=PORTB;
+	LCD1.RS_pin=2;
+	LCD1.E_pin=3;
+	LCD1.data_pin[0]=4;
+	LCD1.data_pin[1]=5;
+	LCD1.data_pin[2]=6;
+	LCD1.data_pin[3]=7;
+	
+   lcd_init(&LCD1);
+   TWI_Init(100,0x01);
+   lcd_cmd(&LCD1,LCD_ON_CURSOR_OFF);
+   lcd_out(&LCD1,1,1,"Start");
+   _delay_ms(100);
+   lcd_out(&LCD1,2,1,_data);
+   TWI_Start();
+   TWI_Write(0b10100000);
+   _delay_ms(500);
+   TWI_Write(0xFF);
+   _delay_ms(500);
+   TWI_Write(0xFE);
+   _delay_ms(500);
+   TWI_Stop();
+   _delay_ms(1000);
+   TWI_Start();
+   TWI_Write(0b10100001);
+  // data=TWI_Read_With_NACK();
+   TWI_Stop();
+  
     while (1) 
     {
     }
