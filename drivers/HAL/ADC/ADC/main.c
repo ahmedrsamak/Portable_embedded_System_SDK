ed03d0253b8b17956baa098c6e4450694481ba83
Ahmed <ahmedrsamak@gmail.com>
2023-02-22 22:21:54 +0200

    Added ADC File to HAL

---
+++ drivers/HAL/ADC/ADC/main.c
@@ -0,0 +1,18 @@
+/*
+ * ADC.c
+ *
+ * Created: 2/22/2023 10:21:11 PM
+ * Author : ahmed
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
